b8f3dd66a1f21a93355aef1ada7dcabd047afed4
Tymur Savchuk <stimurr@gmail.com>
2024-11-20 21:59:30 +0200

    test commit

--- hw04.py
+++ hw04.py
@@ -1,4 +1,7 @@
-with open('text.txt', 'w') as fh:
-    # Виконання операцій з файлом
-    fh.write('Some data')
-# Файл автоматично закриється після виходу з блоку with
+with open("test.txt", "w") as fh:
+    fh.write("first line\nsecond line\nthird line")
+
+with open("test.txt", "r") as fh:
+    lines = [el.strip() for el in fh.readlines()]
+
+print(lines)
